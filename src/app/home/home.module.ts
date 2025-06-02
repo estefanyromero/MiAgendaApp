774ed6bfa2ca799a1460dfca1676dba4cb900765
estefanyromero <este.romero@duocuc.cl>
2025-06-01 20:52:07 -0400

    Agregado paso de login con validación y navegación a Home con usuario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,26 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
+  CommonModule,
+  FormsModule,
+  IonicModule,
+  HomePageRoutingModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatNativeDateModule
+],
   declarations: [HomePage]
 })
 export class HomePageModule {}
