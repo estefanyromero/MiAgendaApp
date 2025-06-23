529c99603a96d2be1271c7fc727d978a3d1a706b
estefanyromero <este.romero@duocuc.cl>
2025-06-22 20:39:44 -0400

    Respaldo antes de actualizar Angular

---
+++ android/app/src/main/java/com/estefany/miagenda/MainActivity.java
@@ -0,0 +1,5 @@
+package com.estefany.miagenda;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.estefany.miagenda',
+  appName: 'MiAgendaApp',
+  webDir: 'www'
+};
+
+export default config;
